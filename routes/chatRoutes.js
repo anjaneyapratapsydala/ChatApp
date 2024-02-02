8076d6f63168b2c1a16ab2a17e058b412c7a2707
anjaneyapratapsydala <140205127+anjaneyapratapsydala@users.noreply.github.com>
2024-02-02 23:29:13 +0530

    Update chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -12,10 +12,10 @@ const { protect } = require("../middleware");
 const router = express.Router();
 
 // Only logged in user can access the below routes
-router.route("/").post(protect, accessChat).get(protect, fetchChats); // Both requests work on same route
-router.route("/group").post(protect, createGroupChat); // Create group chat
-router.route("/rename").put(protect, renameGroup); // Rename group chat
-router.route("/groupadd").put(protect, addToGroup); // Add someone to the group
-router.route("/groupremove").put(protect, removeFromGroup); // Remove someone or leave the group
+router.route("http://localhost:5000").post(protect, accessChat).get(protect, fetchChats); // Both requests work on same route
+router.route("http://localhost:5000/group").post(protect, createGroupChat); // Create group chat
+router.route("http://localhost:5000/rename").put(protect, renameGroup); // Rename group chat
+router.route("http://localhost:5000/groupadd").put(protect, addToGroup); // Add someone to the group
+router.route("http://localhost:5000/groupremove").put(protect, removeFromGroup); // Remove someone or leave the group
 
 module.exports = router;
