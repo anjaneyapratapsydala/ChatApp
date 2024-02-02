53a1611a4cfa9464e9a72fa99563c99dac477041
anjaneyapratapsydala <140205127+anjaneyapratapsydala@users.noreply.github.com>
2024-02-02 23:28:25 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const { protect } = require("../middleware");
 
 const router = express.Router();
 
-router.route("/http://localhost:5000").post(registerUser).get(protect, allUsers); // Both request supported on the same route
-router.post("/http://localhost:5000/login", authUser);
+router.route("http://localhost:5000").post(registerUser).get(protect, allUsers); // Both request supported on the same route
+router.post("http://localhost:5000/login", authUser);
 
 module.exports = router;
